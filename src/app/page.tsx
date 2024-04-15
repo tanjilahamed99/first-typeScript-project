3c2e5f4cced3b65c564f3c31c878508d82ed0adf
Tanjil Ahamed <tanjil113355@gmail.com>
2024-04-15 21:23:28 +0600

    frist commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-"use client";
-
 export default function Home() {
-  const hello = (ami: string) => {
-    console.log(ami);
-  };
-
-  return <div onClick={() => hello('hello')}>hello</div>;
+  return <div >hello ami asi</div>;
 }
